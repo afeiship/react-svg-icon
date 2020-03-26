4f8baa689632bb006e7dcc2c91c1e960a790fd9b
afeiship <1290657123@qq.com>
2020-03-26 13:27:16 +0800

    feat: add size

--- public/index.js
+++ public/index.js
@@ -9,9 +9,17 @@ class App extends React.Component {
     return (
       // copy value from iconfont: https://www.iconfont.cn/manage/index?manage_type=myprojects&projectId=YOURPROJECT_ID
       <div className="app-container">
-        <ReactSvgIcon value="iconPaaS_test" />
-        <ReactSvgIcon value="iconPaaS_search" />
-        <ReactSvgIcon value="iconPaaS_sort" />
+        <div className="is-item">
+          <ReactSvgIcon value="iconPaaS_test" />
+          <ReactSvgIcon value="iconPaaS_search" />
+          <ReactSvgIcon value="iconPaaS_sort" />
+        </div>
+
+        <div className="is-item">
+          <ReactSvgIcon value="iconPaaS_ruler" size="20" />
+          <ReactSvgIcon value="iconG_zhenduan" size="40"/>
+          <ReactSvgIcon value="iconG_data" size="50"/>
+        </div>
       </div>
     );
   }
--- src/components/index.js
+++ src/components/index.js
@@ -22,7 +22,11 @@ export default class ReactSvgIcon extends Component {
     /**
      * The icon name.
      */
-    value: PropTypes.string
+    value: PropTypes.string,
+    /**
+     * The icon size(font-size).
+     */
+    size: PropTypes.number
   };
 
   static defaultProps = {
@@ -30,11 +34,13 @@ export default class ReactSvgIcon extends Component {
   };
 
   render() {
-    const { iconClass, className, value, ...props } = this.props;
+    const { iconClass, className, value, size, style, ...props } = this.props;
+    const _style = objectAssign({ fontSize: size }, style);
     return (
       <svg
         aria-hidden="true"
         data-component={CLASS_NAME}
+        style={_style}
         className={classNames(iconClass, CLASS_NAME, className)}
         {...props}>
         <use xlinkHref={`#${value}`} />
